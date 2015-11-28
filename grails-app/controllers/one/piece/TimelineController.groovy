85d5eec6de1b2ac8d40bb612ed0ca9e8323c42e9
Peter Nancke <peter.nancke@gmx.de>
2015-11-28 20:09:50 +0100

    Add Simple Search Field for Figures

--- grails-app/controllers/one/piece/TimelineController.groovy
+++ grails-app/controllers/one/piece/TimelineController.groovy
@@ -3,5 +3,28 @@ package one.piece
 class TimelineController {
 
     def index() {
+        def query = {
+            if (params.name) {
+                ilike('figName', '%' + params.name + '%')
+            }
+            if (params.sort) {
+                order(params.sort, params.order)
+            }
+        }
+
+        def criteria = Figure.createCriteria()
+        params.max = Math.min(params.max ? params.int('max') : 20, 100)
+        def figures = criteria.list(query, max: params.max, offset: params.offset)
+        def filters = [name: params.name]
+
+        def model = [figureInstanceList: figures, figureInstanceTotal: figures.totalCount, filters: filters]
+
+        if (request.xhr) {
+            // ajax request
+            render(template: "figureList", model: model)
+        } else {
+            model
+        }
     }
+
 }
--- grails-app/views/timeline/index.gsp
+++ grails-app/views/timeline/index.gsp
@@ -3,7 +3,31 @@
 <head>
     <title>timeline</title>
 </head>
+
 <body>
 <h1>One Piece Timeline</h1>
+
+<g:if test="${flash.message}">
+    <div class="message">${flash.message}</div>
+</g:if>
+<br/>
+
+<div class="filters">
+    <g:form action="index">
+
+        <p><label for="name">Name</label>
+            <g:textField name="name" value="${filters?.name}"/>
+
+            <g:submitButton name="search" value="Search"/></p>
+
+    </g:form>
+</div>
+<br/>
+
+<div id="figureList">
+    <g:render template="figureList" model="model"/>
+</div>
+<br/>
+
 </body>
 </html>
