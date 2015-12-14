f6fbaa3634864005364f87e8310f812f26583f46
darrit <xdarritx@gmail.com>
2015-12-14 12:52:52 +0100

    Update DevilFruit.groovy
    remove indentation

--- grails-app/domain/one/piece/DevilFruit.groovy
+++ grails-app/domain/one/piece/DevilFruit.groovy
@@ -2,11 +2,11 @@ package one.piece
 
 class DevilFruit {
     String defName
-	String defMeaning
+    String defMeaning
     String defType
 
     static constraints = {
-		defMeaning nullable: true
+	defMeaning nullable: true
         defType nullable: true
     }
 }
