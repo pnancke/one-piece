db6b252995f4093e560f57503344b1b32b527069
Peter Nancke <peter.nancke@gmx.de>
2015-12-17 12:32:50 +0100

    Fix no episode bug

--- grails-app/controllers/one/piece/TimelineController.groovy
+++ grails-app/controllers/one/piece/TimelineController.groovy
@@ -1,74 +1,71 @@
 package one.piece
 
 import org.grails.web.json.JSONObject
+import com.google.common.base.Joiner
 
 class TimelineController {
+    public static final Joiner EPISODE_JOINER = Joiner.on(" ").skipNulls()
 
-    def index() {
-
-    }
+    def index() {}
 
     def travizDataAnime(String figures) {
-        def figureArray = figures.split(',')
-        def figureEpisodeList = []
-        for (int i = 0; i < figureArray.size(); i++) {
-            def currFigure = Figure.findByFigName(figureArray[i])
-            if (currFigure == null) {
-                log.error("Could not find figure with name: " + figureArray[i])
-            } else {
-                def appearance = currFigure.animeEpisodeAppearance
-                if (appearance != null) {
-                    createAnimeEpisodeString(currFigure, appearance, figureEpisodeList)
-                }
-            }
-        }
-        log.info("travizDataAnime response: " + figureEpisodeList.toString())
-        render figureEpisodeList.toString()
+        def figureNamesList = new ArrayList(Arrays.asList(figures.split(',')))
+        def figureEpisodeMap = getFigureAnimeEpisodeMap(figureNamesList)
+        def figureEpisodeJson = generateTravizString(figureEpisodeMap)
+        render figureEpisodeJson.toString()
     }
 
     def travizDataManga(String figures) {
-        def figureArray = figures.split(',')
-        def figureEpisodeList = []
-        for (int i = 0; i < figureArray.size(); i++) {
-            def currFigure = Figure.findByFigName(figureArray[i])
+        def figureNamesList = new ArrayList(Arrays.asList(figures.split(',')))
+        def figureEpisodeMap = getFigureMangaEpisodeMap(figureNamesList)
+        def figureEpisodeJson = generateTravizString(figureEpisodeMap)
+        render figureEpisodeJson.toString()
+    }
+
+    private static ArrayList generateTravizString(LinkedHashMap figureEpisodeMap) {
+        def result = new ArrayList()
+        for (Map.Entry<String, String> entry : figureEpisodeMap.entrySet()) {
+            if (entry.getKey().isEmpty() || entry.getValue().isEmpty()) {
+                continue
+            }
+            def figEpisodeJsonObject = new JSONObject()
+            figEpisodeJsonObject["edition"] = entry.getKey()
+            def episodes = EPISODE_JOINER.join(entry.getValue())
+            figEpisodeJsonObject["text"] = episodes
+            result << figEpisodeJsonObject.toString()
+        }
+        return result
+    }
+
+    private getFigureAnimeEpisodeMap = { ArrayList rawFigureNames ->
+        def figureEpisodeMap = [:]
+        for (int i = 0; i < rawFigureNames.size(); i++) {
+            def currFigure = Figure.findByFigName(rawFigureNames[i].toString())
             if (currFigure == null) {
-                log.error("Could not find figure with name: " + figureArray[i])
+                log.error("Could not find figure with name: " + rawFigureNames[i])
             } else {
-                def appearance = currFigure.mangaEpisodeAppearance
+                def appearance = currFigure.animeEpisodeAppearance
                 if (appearance != null) {
-                    createMangaEpisodeString(currFigure, appearance, figureEpisodeList)
+                    figureEpisodeMap.put(currFigure.figName, appearance.aneNumber.sort())
                 }
             }
         }
-        log.info("travizDataManga response: " + figureEpisodeList.toString())
-        render figureEpisodeList.toString()
+        return figureEpisodeMap
     }
 
-    private static void createMangaEpisodeString(Figure currFigure, Set<MangaEpisode> appearance,
-                                                 ArrayList figureEpisodeList) {
-        def sortedAppearance = appearance.toSorted { a, b -> a.maeNumber <=> b.maeNumber }
-        def figEpisodeJsonObject = new JSONObject()
-        figEpisodeJsonObject["edition"] = currFigure.getFigName()
-        def figEpisodeNumber = new StringBuilder()
-        for (int j = 0; j < sortedAppearance.size(); j++) {
-            figEpisodeNumber.append(sortedAppearance[j].getMaeNumber() + " ")
+    private getFigureMangaEpisodeMap = { ArrayList rawFigureNames ->
+        def figureEpisodeMap = [:]
+        for (int i = 0; i < rawFigureNames.size(); i++) {
+            def currFigure = Figure.findByFigName(rawFigureNames[i].toString())
+            if (currFigure == null) {
+                log.error("Could not find figure with name: " + rawFigureNames[i])
+            } else {
+                def appearance = currFigure.mangaEpisodeAppearance
+                if (appearance != null) {
+                    figureEpisodeMap.put(currFigure.figName, appearance.maeNumber.sort())
+                }
+            }
         }
-        figEpisodeNumber.deleteCharAt(figEpisodeNumber.length() - 1)
-        figEpisodeJsonObject["text"] = figEpisodeNumber.toString()
-        figureEpisodeList << figEpisodeJsonObject.toString()
-    }
-
-    private static void createAnimeEpisodeString(Figure currFigure, Set<AnimeEpisode> appearance,
-                                                 ArrayList figureEpisodeList) {
-        def sortedAppearance = appearance.toSorted { a, b -> a.aneNumber <=> b.aneNumber }
-        def figEpisodeJsonObject = new JSONObject()
-        figEpisodeJsonObject["edition"] = currFigure.getFigName()
-        def figEpisodeNumber = new StringBuilder()
-        for (int j = 0; j < sortedAppearance.size(); j++) {
-            figEpisodeNumber.append(sortedAppearance[j].getAneNumber() + " ")
-        }
-        figEpisodeNumber.deleteCharAt(figEpisodeNumber.length() - 1)
-        figEpisodeJsonObject["text"] = figEpisodeNumber.toString()
-        figureEpisodeList << figEpisodeJsonObject.toString()
+        return figureEpisodeMap
     }
 }
