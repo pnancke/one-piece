2146c242d25149c42f397e658ddd917b4d51fe4d
Peter Nancke <peter.nancke@gmx.de>
2016-01-31 16:22:54 +0100

    Add figure box popup

--- grails-app/controllers/one/piece/TimelineController.groovy
+++ grails-app/controllers/one/piece/TimelineController.groovy
@@ -33,78 +33,77 @@ class TimelineController {
         }
         render result.toString()
     }
-	
-	def getFigureInformation(String term){
-		def data = [:]
-		def resultCount = 0
-		
-		def successResponse = true
-		log.info("search for: " + term)
-		
-		if (term == null) {
-			successResponse = false
-		} else if (term.contains(' (Group)')) {
-			def entity = Figure.getGroup(term.minus(' (Group)')).minus(' (Group)')
-			data.put("Group", entity)
-			resultCount = entity.size()
-			
-		} else if (term.contains(' (Figure)')) {
-			def entity = term.minus(' (Figure)')
-			def figure = Figure.findByFigNameIlike(entity)
-			if (figure == null) {
-				successResponse = false
-			} else {
 
-				if(figure.figPicture != null)
-					data.put("Picture", figure.figPicture)
-				if(figure.figName != null)
-					data.put("Name", figure.figName)
-				if(figure.figRace != null)
-					data.put("Race", figure.figRace)
-				if(figure.figAge != null)
-					data.put("Age", figure.figAge)
-				if(figure.figStatus != null)
-					data.put("Status", figure.figStatus)
-				if(figure.figOrigin != null)
-					data.put("Origin", figure.figOrigin)
-				
-				if(figure.figMarine != null){
-					Marine mari = figure.figMarine
-					data.put("Occupations", mari.marRank)
-				}
-				
-				if(figure.figPirate != null){
-					Pirate pir = figure.figPirate
-					data.put("Position", pir.pirPosition)
-					data.put("Bounty", pir.pirBounty)
-				}
-					
-				if(figure.devilFruit != null){
-					DevilFruit devf = figure.devilFruit
-					data.put("Devilfruit", devf.defName)
-				}
-				
-				resultCount = 1;
-				log.info("HASHMAP    "+data)
-				
-			}
-		} else if (term.endsWith(' (Attribute)')) {
-			def attribute = term.minus(' (Attribute)')
-			def criteria = Figure.createCriteria()
-			Closure query = Figure.createWhereQuery(attribute)
-			def figures = criteria.list(query)
-			if (figures == null || figures.isEmpty()) {
-				successResponse = false
-			} else {
-				data.put("Attribute",figures)
-				resultCount = figures.size()
-			}
-		} else {
-		successResponse = false
-		}
+    def getFigureInformation(String term) {
+        def data = [:]
+        def resultCount = 0
 
-		def response = HttpUtils.buildJsonResponse(successResponse, data, resultCount).toString()
-		render response
-	}
-	
+        def successResponse = true
+        log.info("get figure information for: " + term)
+
+        if (term == null) {
+            successResponse = false
+        } else if (term.contains(' (Group)')) {
+            def entities = Figure.getGroup(term.minus(' (Group)')).minus(' (Group)')
+            if (entities != null) {
+                data.put("Members", entities.join(', '))
+            }
+            resultCount = entities.size()
+
+        } else if (term.contains(' (Figure)')) {
+            def entity = term.minus(' (Figure)')
+            def figure = Figure.findByFigNameIlike(entity)
+            if (figure == null) {
+                successResponse = false
+            } else {
+                if (figure.figPicture != null)
+                    data.put("Picture", figure.figPicture)
+                if (figure.figName != null)
+                    data.put("Name", figure.figName)
+                if (figure.figRace != null)
+                    data.put("Race", figure.figRace)
+                if (figure.figAge != null)
+                    data.put("Age", figure.figAge)
+                if (figure.figStatus != null)
+                    data.put("Status", figure.figStatus)
+                if (figure.figOrigin != null)
+                    data.put("Origin", figure.figOrigin)
+                if (figure.figMarine != null) {
+                    Marine mari = figure.figMarine
+                    data.put("Occupations", mari.marRank)
+                }
+                if (figure.figPirate != null) {
+                    Pirate pir = figure.figPirate
+                    data.put("Position", pir.pirPosition)
+                    data.put("Bounty", pir.pirBounty)
+                }
+                if (figure.devilFruit != null) {
+                    DevilFruit devf = figure.devilFruit
+                    data.put("Devilfruit", devf.defName)
+                }
+
+                resultCount = 1;
+
+            }
+        } else if (term.endsWith(' (Attribute)')) {
+            def attribute = term.minus(' (Attribute)')
+            def criteria = Figure.createCriteria()
+            Closure query = Figure.createWhereQuery(attribute)
+            def figures = criteria.list(query)
+            if (figures == null || figures.isEmpty()) {
+                successResponse = false
+            } else {
+                data.put("Attribute", figures)
+                resultCount = figures.size()
+            }
+        } else {
+            successResponse = false
+        }
+
+        data.put("SearchTerm", term)
+
+        def response = HttpUtils.buildJsonResponse(successResponse, data, resultCount).toString()
+        render response
+    }
+
 }
